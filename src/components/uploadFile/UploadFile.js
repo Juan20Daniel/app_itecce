6ce0b242a501dcbeff5a0b6d687e499f8185230c
Juan20Daniel <juandaniel312000@gmail.com>
2025-05-19 09:46:44 -0600

    Se agregó la nota de los campos obligatorios en la sección de Agregar personal

--- src/components/uploadFile/UploadFile.js
+++ src/components/uploadFile/UploadFile.js
@@ -1,11 +1,31 @@
+import { useContext, useLayoutEffect, useState } from 'react';
 import BtnUploadFile from './components/btnUploadFile/BtnUploadFile';
+import SectionOptionsContext from '../../context/sectionOptions/SectionOptionsContext';
 import './uploadFile.css';
-const UploadFile = () => (
-    <div className="upload-file">
-        <h2>Selecciona la lista a cargar</h2>
-        <p>Selecciona la lista de alumnos, profesores o colaboradores en Excel, haciendo clic en el botón azul.</p>
-        <BtnUploadFile />
-    </div>
-);
+
+const fileStructures = {
+    "Estudiantes": 'Sección | Grupo | Matrícula | Nombre | Apellido paterno | Apellido materno',
+    "Profesores": 'Matrícula | Nombre | Apellido paterno | Apellido materno',
+    "Colaboradores": 'Matrícula | Nombre | Apellido paterno | Apellido materno'
+}
+
+const UploadFile = () => {
+    const [ typeFile, setTypeFile ] = useState('');
+    const { options } = useContext(SectionOptionsContext);
+    useLayoutEffect(() => {
+        const optionSelected = options.find(option => option.active);
+        setTypeFile(optionSelected.name);
+    },[options]);
+    return (
+        <div className="upload-file">
+            <h2>Selecciona la lista a cargar</h2>
+            <div className='box-note'>
+                <p className='note'>Verifica que la lista de <b>"{typeFile}"</b> cumpla con la siguiente estructura:</p>
+                <p className='note'>{fileStructures[typeFile]}</p>
+            </div>
+            <BtnUploadFile />
+        </div>
+    )
+};
 
 export default UploadFile;
